d6a72a35457abed97415990992e50997d00622be
Will Harding <harding.will@gmail.com>
2021-12-22 13:48:40 -0800

    addition and subtraction working

--- compiler.scm
+++ compiler.scm
@@ -1,13 +1,34 @@
-(import (scheme small))
 
-(define input (read))
+(import (scheme small)
+	(srfi 115)
+	(srfi 166))
 
-;(display input)
-;(display "\n")
-(write-string (string-append "
-    .globl main\n
-main:
-    mov $" (number->string input) ", %rax
-    ret
-"))
+(define (read-token token)
+  (call-with-port (open-input-string token)
+		  (lambda (p) (read p))))
 
+(define (parse)
+  (let* ((l (read-line))
+	 (tokens (regexp-partition '(or "+" "-") l)))
+    (map read-token tokens)))
+
+(define (emit input)
+  (show #t "  .globl main\n")
+  (show #t "main:\n")
+  (show #t "  mov $" (car input) ", %rax\n")
+  (let loop ((ins (cdr input)))
+    (cond 
+     ((null? ins) #f)
+     ((equal? (car ins) '+)
+      (begin (show #t "  add $" (cadr ins) ", %rax\n")
+	     (loop (cddr ins))))
+      ((equal? (car ins) '-)
+       (begin (show #t "  sub $" (cadr ins) ", %rax\n")
+	      (loop (cddr ins))))))
+  (show #t "ret\n"))
+
+(define (main)
+  (let ((input (parse)))
+    (emit input)))
+
+(main)
